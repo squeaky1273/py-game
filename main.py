929320450c87f22b425ad11f9161a4c18d0aa842
squeaky1273 <pjriddell22@gmail.com>
2020-10-13 14:47:27 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Create Window
+screen = pygame.display.set_mode((800, 600))
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
