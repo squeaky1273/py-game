ef030b10ad03a70185c5332f44dd96c0a442831e
squeaky1273 <pjriddell22@gmail.com>
2020-10-13 21:37:15 -0700

    added code for game over text/show score

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 
 # Create Window
 screen = pygame.display.set_mode((800, 600))
+WHITE = (255, 255, 255)
+screen.fill(WHITE)
 
 # Player
 player_x = 50
@@ -18,10 +20,25 @@ enemy_y = []
 enemy_change = []
 
 # Score
-score = 0
+score_number = 0
+score_font = pygame.font.Font(None, 30)
 
 # Game Over
+game_over_font = pygame.font.Font(None, 60)
 
+def game_over_text():
+    game_over_text = game_over_font.render("Game Over", True, WHITE)
+    screen.blit(game_over_text, (200, 250))
+
+def show_score(x, y):
+    score = font.render("Score : " + str(score_number), True, WHITE)
+    screen.blit(score, (x,y))
+
+def player(x, y):
+    pass
+
+def enemy():
+    pass
 
 # Game Loop
 running = True
