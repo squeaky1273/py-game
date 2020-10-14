c538457667f86c4a446ce47cfd893f8a8ffacfc7
squeaky1273 <pjriddell22@gmail.com>
2020-10-13 20:05:53 -0700

    added starter player and enemy code

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ pygame.init()
 # Create Window
 screen = pygame.display.set_mode((800, 600))
 
+# Player
+player_x = 50
+player_y = 50
+player_change = 0
+
+# Enemy
+enemy_x = []
+enemy_y = []
+enemy_change = []
+
+# Score
+score = 0
+
+# Game Over
+
+
 # Game Loop
 running = True
 while running:
